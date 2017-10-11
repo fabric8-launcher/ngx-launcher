99c7df38e3429157c37919728bee625c154f3dba
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-10-11 16:15:02 +0200

    ng module

--- index.ts
+++ index.ts
@@ -1,8 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AsciidocComponent } from './src/app/asciidoc/asciidoc.component';
+import { InputComponent } from './src/app/input/input.component';
+import { ProjectSelect } from './src/app/project-select/project-select';
+import { Config } from './src/app/service/config.component';
+import { ForgeService } from './src/app/service/forge.service';
+import { History } from './src/app/service/history.component';
 
-export { AsciidocComponent } from './src/app/asciidoc/asciidoc.component';
-export { InputComponent } from './src/app/input/input.component';
 export * from './src/app/model/base.model';
 export * from './src/app/model/gui.model';
-export { ProjectSelect } from './src/app/project-select/project-select';
-export { ForgeService } from './src/app/service/forge.service';
-export { History } from './src/app/service/history.component';
+
+const classes = [
+  AsciidocComponent,
+  InputComponent,
+  ProjectSelect,
+  Config,
+  ForgeService,
+  History
+];
+
+@NgModule({
+  imports: [CommonModule, FormsModule],
+  exports: classes,
+  declarations: classes
+})
+export class NgxForgeModule {
+}
+
