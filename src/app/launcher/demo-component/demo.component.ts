a02058b6a70651ab7a1e7cc030a2317c4ebdf0fa
Dana Gutride <dgutride@redhat.com>
2018-01-12 12:43:18 -0500

    chore(scroll): add scroll watch to window

--- src/app/launcher/demo-component/demo.component.ts
+++ src/app/launcher/demo-component/demo.component.ts
@@ -8,4 +8,15 @@ import { Component, Input } from '@angular/core';
 export class DemoComponent {
 
   @Input() testValue: string;
+  selectedSection: string;
+
+  onInViewportChange($event: boolean, id: string) {
+    if ($event) {
+      this.selectedSection = id;
+    }
+  }
+
+  selectSection(id: string) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
