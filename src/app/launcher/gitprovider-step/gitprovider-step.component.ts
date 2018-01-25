0fcb27560b33bda4be42673109c3217edd9adf14
Dan Labrecque <dlabrecq@redhat.com>
2018-01-25 17:03:00 -0500

    chore(gitprovider): added auth functionality to demo
    Note: To test, enable Access-Conrtol-Expose-Headers for CORS

--- src/app/launcher/gitprovider-step/gitprovider-step.component.ts
+++ src/app/launcher/gitprovider-step/gitprovider-step.component.ts
@@ -1,8 +1,18 @@
-import { Component, Host, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Host, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { GitProviderService } from '../service/gitprovider.service';
 import { Selection } from '../model/selection.model';
 import { WizardComponent } from '../wizard.component';
+
+import {
+  GitHubRepo,
+  GitHubRepoCommit,
+  GitHubRepoDetails,
+  GitHubRepoLastCommit,
+  GitHubRepoLicense,
+  GitHubUser
+} from '../model/github.model';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -10,12 +20,39 @@ import { WizardComponent } from '../wizard.component';
   templateUrl: './gitprovider-step.component.html',
   styleUrls: ['./gitprovider-step.component.less']
 })
-export class GitProviderStepComponent implements OnInit {
+export class GitProviderStepComponent implements OnDestroy, OnInit {
+  private _ghAvatar: string;
+  private _ghLogin: string;
+  private _ghToken: string;
+  private subscriptions: Subscription[] = [];
+  private ghUser: GitHubUser;
+
   constructor(@Host() public wizardComponent: WizardComponent,
               private gitProviderService: GitProviderService) {
   }
 
   ngOnInit() {
+    this.subscriptions.push(
+      this.gitProviderService.getToken().subscribe((val) => {
+        if (val !== undefined) {
+          this._ghToken = val;
+        }
+      }));
+
+    this.subscriptions.push(
+      this.gitProviderService.getUser().subscribe((val) => {
+        if (val !== undefined) {
+          this.ghUser = val;
+          this._ghAvatar = this.ghUser.avatar_url;
+          this._ghLogin = this.ghUser.login;
+        }
+      }));
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => {
+      sub.unsubscribe();
+    });
   }
 
   /**
@@ -23,15 +60,55 @@ export class GitProviderStepComponent implements OnInit {
    *
    * @param {MouseEvent} $event
    */
-  authorize($event: MouseEvent): void {
+  authorizeAccount($event: MouseEvent): void {
     let url = window.location.origin + this.getParams(this.wizardComponent.selection);
     this.gitProviderService.authorize(url);
   }
+
+  /**
+   * Change GitHub account
+   *
+   * @param {MouseEvent} $event
+   */
+  changeAccount($event: MouseEvent): void {
+    let url = window.location.origin + this.getParams(this.wizardComponent.selection);
+    this.gitProviderService.authorize(url);
+  }
+
+  // Accessors
+
+  get ghAvatar(): string {
+    return this._ghAvatar;
+  }
+
+  get ghLogin(): string {
+    return this._ghLogin;
+  }
+
+  get ghToken(): string {
+    return this._ghToken;
+  }
+
+  // Private
 
   private getParams(selection: Selection) {
     if (selection === undefined) {
       return '';
     }
     return '?selection=' + JSON.stringify(selection);
+  }
+
+  /**
+   * Helper to retrieve request parameters
+   *
+   * @param name The request parameter to retrieve
+   * @returns {any} The request parameter value or null
+   */
+  private getRequestParam(name: string): string {
+    let param = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(window.location.search);
+    if (param !== null) {
+      return decodeURIComponent(param[1]);
+    }
+    return null;
   }
 }
--- src/app/launcher/service/gitprovider.service.ts
+++ src/app/launcher/service/gitprovider.service.ts
@@ -1,3 +1,14 @@
+import { Observable } from 'rxjs';
+
+import {
+  GitHubRepo,
+  GitHubRepoCommit,
+  GitHubRepoDetails,
+  GitHubRepoLastCommit,
+  GitHubRepoLicense,
+  GitHubUser
+} from '../model/github.model';
+
 /**
  * Abstract GitHub provider service provided to ensure consumer implements this pattern
  */
@@ -8,4 +19,26 @@ export abstract class GitProviderService {
    * @param {string} redirectUrl The URL to return back to from GitHub
    */
   abstract authorize(redirectUrl: string): void;
+
+  /**
+   * Get GitHub repo details for given full name
+   *
+   * @param fullName The GitHub full name (e.g., fabric8-services/fabric8-wit)
+   * @returns {Observable<GitHubRepoDetails>}
+   */
+  abstract getRepoDetailsByFullName(fullName: string): Observable<GitHubRepoDetails>;
+
+  /**
+   * Get authenticate GitHub user
+   *
+   * @returns {Observable<GitHubUser>}
+   */
+  abstract getUser(): Observable<GitHubUser>;
+
+  /**
+   * Get authentication token
+   *
+   * @returns {Observable<any>}
+   */
+  abstract getToken(): Observable<any>;
 }
