25dd59ff259890af3c52922df97b9330e0e3a236
Dan Labrecque <dlabrecq@redhat.com>
2018-02-09 09:24:43 -0500

    chore(app-launcher): added model and service exports to index.js

--- index.ts
+++ index.ts
@@ -19,13 +19,24 @@ export { TokenProvider } from './src/app/service/token-provider';
 export { ForgeErrorsComponent } from './src/app/components/forge-errors/forge-errors.component';
 export { ForgeExceptionComponent } from './src/app/components/forge-exception/forge-exception.component';
 
-// Launcher
+// Launcher models
 export { LauncherModule } from './src/app/launcher/launcher.module';
 export { Cluster } from './src/app/launcher/model/cluster.model';
+export { DependencyCheck } from './src/app/launcher/model/dependency-check.model';
+export { GitHubDetails } from './src/app/launcher/model/github-details.model';
 export { Mission } from './src/app/launcher/model/mission.model';
 export { Pipeline } from './src/app/launcher/model/pipeline.model';
+export { Progress } from './src/app/launcher/model/progress.model';
 export { Runtime } from './src/app/launcher/model/runtime.model';
+export { Summary } from './src/app/launcher/model/summary.model';
+export { TargetEnvironment } from './src/app/launcher/model/target-environment.model';
+
+// Launcher services
 export { ClusterService } from './src/app/launcher/service/cluster.service';
+export { DependencyCheckService } from './src/app/launcher/service/dependency-check.service';
 export { GitProviderService } from './src/app/launcher/service/git-provider.service';
-export { PipelineService } from './src/app/launcher/service/pipeline.service';
 export { MissionRuntimeService } from './src/app/launcher/service/mission-runtime.service';
+export { PipelineService } from './src/app/launcher/service/pipeline.service';
+export { ProjectProgressService } from './src/app/launcher/service/project-progress.service';
+export { ProjectSummaryService } from './src/app/launcher/service/project-summary.service';
+export { TargetEnvironmentService } from './src/app/launcher/service/target-environment.service';
