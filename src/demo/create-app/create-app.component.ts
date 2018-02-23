33f2d3bbf1d27e409226d3c224a69106e0e76723
Dan Labrecque <dlabrecq@redhat.com>
2018-02-23 16:10:58 -0500

    chore(app-launcher): added cancel functionality

--- src/demo/create-app/create-app.component.ts
+++ src/demo/create-app/create-app.component.ts
@@ -2,15 +2,20 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'create-app',
   templateUrl: './create-app.component.html'
 })
 export class CreateAppComponent implements OnInit {
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  cancel(): void {
+    this.router.navigate(['/']);
   }
 }
--- src/demo/import-app/import-app.component.ts
+++ src/demo/import-app/import-app.component.ts
@@ -2,15 +2,20 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'import-app',
   templateUrl: './import-app.component.html'
 })
 export class ImportAppComponent implements OnInit {
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  cancel(): void {
+    this.router.navigate(['/']);
   }
 }
