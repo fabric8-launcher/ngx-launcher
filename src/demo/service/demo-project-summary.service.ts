5d462266091771ab898efe7b128d4440fa6e8611
invinciblejai <mailjai.vardhan@gmail.com>
2018-03-07 15:21:50 +0530

    removes unneeded model
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/demo/service/demo-project-summary.service.ts
+++ src/demo/service/demo-project-summary.service.ts
@@ -75,11 +75,13 @@ export class DemoProjectSummaryService implements ProjectSummaryService {
 
   private handleError(error: Response | any) {
     // In a real world app, we might use a remote logging infrastructure
-    let errMsg: string;
+    let errMsg: string = '';
     if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+      if (error.status !== 401) {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+      }
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
