08f3bbf9c1ece162a039963da19eb8a992e73fdc
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-04-05 13:27:07 +0530

    persists values related to dependency editor on screen reload for Github login

--- src/app/launcher/model/dependency-check.model.ts
+++ src/app/launcher/model/dependency-check.model.ts
@@ -1,7 +1,9 @@
+import { DependencySnapshotItem } from './dependency-snapshot-item.model';
 export class DependencyCheck {
   groupId: string;
   mavenArtifact: string;
   projectName: string;
   projectVersion: string;
   spacePath: string;
+  dependencySnapshot: DependencySnapshotItem;
 }
