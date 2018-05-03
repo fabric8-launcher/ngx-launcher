51df5e6d3803d40b2679612c913191fdeee7210d
sanketpathak <sanketpathak95@gmail.com>
2018-05-03 15:22:31 +0530

    edited payload for summary api

--- src/demo/service/demo-project-summary.service.ts
+++ src/demo/service/demo-project-summary.service.ts
@@ -23,5 +23,42 @@ export class DemoProjectSummaryService implements ProjectSummaryService {
     return Observable.of({});
   }
 
+  private handleError(error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string = '';
+    if (error instanceof Response) {
+      if (error.status !== 401) {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+      }
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  private getPayload(summary: Summary, spaceId: string, spaceName: string) {
+    let payload =
+    'missionId=' + summary.mission.id +
+    '&runtimeId=' + summary.runtime.id +
+    '&runtimeVersion=' + summary.runtime.version.id +
+    '&pipelineId=' + summary.pipeline.id +
+    '&projectName=' + summary.dependencyCheck.projectName +
+    '&projectVersion=' + summary.dependencyCheck.projectVersion +
+    '&groupId=' + summary.dependencyCheck.groupId +
+    '&artifactId=' + summary.dependencyCheck.mavenArtifact +
+    '&spacePath=' + spaceName +
+    '&gitRepository=' + summary.gitHubDetails.repository +
+    '&spaceId=' + spaceId;
+    summary.dependencyEditor.dependencySnapshot.forEach(i => {
+      payload += '&dependency=' + i.package + ':' + i.version;
+    });
+    if (summary.gitHubDetails.login !== summary.gitHubDetails.organization) {
+      payload += '&gitOrganization=' + summary.gitHubDetails.organization;
+    }
+    return payload;
+  }
 
 }
