17b1d88945a854274a7e77572b771c4a42500225
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-05-04 15:15:29 +0200

    fix: return dummy data

--- src/demo/service/demo-token.service.ts
+++ src/demo/service/demo-token.service.ts
@@ -10,59 +10,54 @@ import { Cluster } from '../../app/launcher/model/cluster.model';
 @Injectable()
 export class DemoTokenService implements TokenService {
 
-  private END_POINT: string = '';
-  private API_BASE: string = 'services/openshift/clusters/all';
-  private ORIGIN: string = '';
-
-  constructor(
-    private http: Http,
-    private helperService: HelperService,
-    private tokenProvider: TokenProvider
-  ) {
-    if (this.helperService) {
-      this.END_POINT = this.helperService.getBackendUrl();
-      this.ORIGIN = this.helperService.getOrigin();
-    }
-  }
-
-  private get options(): Observable<RequestOptions> {
-    let headers = new Headers();
-    headers.append('X-App', this.ORIGIN);
-    headers.append('X-Git-Provider', 'GitHub');
-    headers.append('X-Execution-Step-Index', '0');
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-    return Observable.fromPromise(this.tokenProvider.token.then((token) => {
-      headers.append('Authorization', 'Bearer ' + token);
-      return new RequestOptions({
-        headers: headers
-      });
-    }));
-  }
-
   get availableClusters(): Observable<Cluster[]> {
-    return Observable.of([]);
+    return Observable.of([
+      {
+        "id": "starter-ca-central-1",
+        "type": "starter"
+      },
+      {
+        "id": "starter-us-west-1",
+        "type": "starter"
+      }
+    ]);
   };
 
   get clusters(): Observable<Cluster[]> {
-    const endPoint: string = this.END_POINT + this.API_BASE;
-    return this.options.flatMap((option) => {
-      return this.http.get(endPoint, option)
-                  .map(response => response.json() as Cluster[])
-                  .catch(this.handleError);
-    });
-  }
-
-  private handleError(error: Response | any) {
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
+    return Observable.of([
+      {
+        "id": "starter-us-east-1",
+        "type": "starter"
+      },
+      {
+        "id": "starter-us-west-1",
+        "type": "starter"
+      },
+      {
+        "id": "starter-us-west-2",
+        "type": "starter"
+      },
+      {
+        "id": "starter-ca-central-1",
+        "type": "starter"
+      },
+      {
+        "id": "pro-us-east-1",
+        "type": "pro"
+      },
+      {
+        "id": "pro-eu-west-1",
+        "type": "pro"
+      },
+      {
+        "id": "pro-ap-southeast-2",
+        "type": "pro"
+      },
+      {
+        "id": "online-stg",
+        "type": "internal"
+      }
+    ]);
   }
 
   createOathLink(cluster: string): string {
