f36aaa5970f72d746db1370bb8dd63e2f73d6feb
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-05-23 15:01:46 +0200

    added bounce time

--- src/app/launcher/create-app/gitprovider-createapp-step/gitprovider-repository.validator.ts
+++ src/app/launcher/create-app/gitprovider-createapp-step/gitprovider-repository.validator.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core';
 import { NG_ASYNC_VALIDATORS, Validator, ValidatorFn, AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 import { GitProviderService } from '../../service/git-provider.service';
 
@@ -12,19 +13,23 @@ export class GitProviderRepositoryValidatorDirective implements Validator {
 
   constructor(private gitProvider: GitProviderService) {}
 
-  validate( control : AbstractControl ) : Promise<{[key : string] : any}> {
-    return this.validRepositoryName(control);
+  validate( control : AbstractControl ) : Observable<{[key : string] : any}> {
+    return Observable.timer(500).switchMap(()=>{
+        return this.validRepositoryName(control)
+        .mapTo(null)
+        .catch(err=>Observable.of({availability: true}));
+    });
   }
 
-  validRepositoryName(control: AbstractControl): Promise<{[key : string] : any}> {
-    return new Promise(resolve => {
+  validRepositoryName(control: AbstractControl): Observable<{[key : string] : any}> {
+    return new Observable((resolve) => {
       const valid = this.pattern.test(control.value);
       const org = control.parent.get("ghOrg").value;
       if (!valid) {
-        resolve(this.createError(control.value, 'pattern', valid));
+        resolve.next(this.createError(control.value, 'pattern', valid));
       } else if (org) {
         this.gitProvider.isGitHubRepo(org, control.value).subscribe(
-          duplicate => resolve(this.createError(control.value, 'duplicate', !duplicate))
+          duplicate => resolve.next(this.createError(control.value, 'duplicate', !duplicate))
         );
       }
     });
