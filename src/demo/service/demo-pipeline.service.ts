bd14293480c01ac1e3e6258a0eeb11236b2218f4
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-06-04 09:53:13 +0200

    fix: moved mock data to json files, for better maintaince.

--- src/demo/service/demo-pipeline.service.ts
+++ src/demo/service/demo-pipeline.service.ts
@@ -8,61 +8,11 @@ import { PipelineService } from '../../app/launcher/service/pipeline.service';
 import { HelperService } from '../../app/launcher/service/helper.service';
 import { TokenProvider } from '../../app/service/token-provider';
 
+const MockPipeline = require('../../assets/mock/demo-pipeline.json');
+
 @Injectable()
 export class DemoPipelineService implements PipelineService {
   getPipelines(): Observable<Pipeline[]> {
-    let pipelines = Observable.of([{
-      'id': 'maven-release',
-      'platform': 'maven',
-      'name': 'Release',
-      'stages': [{
-        'name': 'Build Release',
-        'description': 'Creates a new version then builds and deploys the project into the maven repository'
-      }, {
-        'name': 'Integration Test',
-        'description': 'Runs an integration test in the **Test** environment'
-      }],
-      'suggested': false,
-      'techpreview': false
-    }, {
-      'id': 'maven-releaseandstage',
-      'platform': 'maven',
-      'name': 'Release and Stage',
-      'stages': [{
-        'name': 'Build Release',
-        'description': 'Creates a new version then builds and deploys the project into the maven repository'
-      }, {
-        'name': 'Integration Test',
-        'description': 'Runs an integration test in the **Test** environment'
-      }, {
-        'name': 'Rollout to Stage',
-        'description': 'Stages the new version into the **Stage** environment'
-      }],
-      'suggested': false,
-      'techpreview': false
-    }, {
-      'id': 'maven-releasestageapproveandpromote',
-      'platform': 'maven',
-      'name': 'Release, Stage, Approve and Promote',
-      'stages': [{
-        'name': 'Build Release',
-        'description': 'Creates a new version then builds and deploys the project into the maven repository'
-      }, {
-        'name': 'Integration Test',
-        'description': 'Runs an integration test in the **Test** environment'
-      }, {
-        'name': 'Rollout to Stage',
-        'description': 'Stages the new version into the **Stage** environment'
-      }, {
-        'name': 'Approve',
-        'description': 'Waits for **Approval** to promote'
-      }, {
-        'name': 'Rollout to Run',
-        'description': 'Promotes to the **Run** environment'
-      }],
-      'suggested': true,
-      'techpreview': false
-    }] as Pipeline[]);
-    return pipelines;
+    return Observable.of(MockPipeline);
   }
 }
--- src/demo/service/demo-token.service.ts
+++ src/demo/service/demo-token.service.ts
@@ -4,52 +4,12 @@ import {Observable} from 'rxjs';
 import {TokenService} from '../../app/launcher/service/token.service';
 import {Cluster} from '../../app/launcher/model/cluster.model';
 
+const MockClusters = require('../../assets/mock/demo-token.json');
 @Injectable()
 export class DemoTokenService implements TokenService {
 
   get clusters(): Observable<Cluster[]> {
-    return Observable.of([
-      {
-        'id': 'starter-us-east-1',
-        'type': 'starter',
-        'connected': true
-      },
-      {
-        'id': 'starter-us-west-1',
-        'type': 'starter',
-        'connected': false
-      },
-      {
-        'id': 'starter-us-west-2',
-        'type': 'starter',
-        'connected': false
-      },
-      {
-        'id': 'starter-ca-central-1',
-        'type': 'starter',
-        'connected': false
-      },
-      {
-        'id': 'pro-us-east-1',
-        'type': 'pro',
-        'connected': false
-      },
-      {
-        'id': 'pro-eu-west-1',
-        'type': 'pro',
-        'connected': false
-      },
-      {
-        'id': 'pro-ap-southeast-2',
-        'type': 'pro',
-        'connected': false
-      },
-      {
-        'id': 'online-stg',
-        'type': 'internal',
-        'connected': false
-      }
-    ]);
+    return Observable.of(MockClusters);
   }
 
   createOathLink(cluster: string): string {
