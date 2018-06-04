a9b426ddf1137f51755688e4a8375d8c2390f2b5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-06-04 11:44:50 +0200

    chore: removed components that are no longer used (#241)

--- index.ts
+++ index.ts
@@ -1,20 +1,3 @@
-export { SpinnerComponent } from './src/app/components/spinner/spinner.component';
-export { PipelineViewComponent } from './src/app/components/pipeline-view/pipeline-view.component';
-export { MultipleSelectionListComponent } from './src/app/components/multiple-selection-list/multiple-selection-list.component';
-export { SelectedItemsPipe } from './src/app/components/multiple-selection-list/selected-items.pipe';
-export { VisibleItemsPipe } from './src/app/components/multiple-selection-list/visible-items.pipe';
-export { SingleSelectionDropDownComponent } from './src/app/components/single-selection-dropdown/single-selection-dropdown.component';
-export { InputComponent } from './src/app/components/input/input.component';
-export { ProjectSelect, ProjectSelectConfig } from './src/app/components/project-select/project-select.component';
-export * from './src/app/model/base.model';
-export { Config } from './src/app/service/config.component';
-export { ForgeService } from './src/app/service/forge.service';
-export { History } from './src/app/service/history.component';
-export { NgxForgeModule } from './ngx-forge.module';
-export { TokenProvider } from './src/app/service/token-provider';
-export { ForgeErrorsComponent } from './src/app/components/forge-errors/forge-errors.component';
-export { ForgeExceptionComponent } from './src/app/components/forge-exception/forge-exception.component';
-
 // Launcher models
 export { LauncherModule } from './src/app/launcher/launcher.module';
 export { Cluster } from './src/app/launcher/model/cluster.model';
